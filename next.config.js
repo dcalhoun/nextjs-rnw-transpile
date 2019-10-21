8e790138ebfdf21674eabf253fbc49164fea236c
David Calhoun <dpcalhoun@gmail.com>
2019-10-21 09:16:09 -0500

    Simplify configuration after upgrading to Next 9
    Something has changed between version of Next that addressed the
    original issue. I have not researched heavily enough to determine where
    that fix occurred.

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
 const withTM = require("next-transpile-modules");
 
 module.exports = withTM({
-  transpileModules: ["react-native-web"]
+  transpileModules: ["react-native-web"],
+  webpack: config => {
+    // Alias all `react-native` imports to `react-native-web`
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      "react-native$": "react-native-web"
+    };
+
+    return config;
+  }
 });
